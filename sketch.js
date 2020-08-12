1a1a90df43638589c839c546bf7ffd8e166b4dc7
Headless-Omega <68866303+Headless-Omega@users.noreply.github.com>
2020-08-12 16:55:22 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,12 +7,11 @@ function setup() {
   createCanvas(1600,400);
   speed=random(223,321);
   weight=random(30,52);
+  thickness = random(22, 83);
   wall=createSprite(700, 200, thickness, height/2);
   bullet = createSprite(50,200,50,50);
 
   damage=0;
-
-  thickness = random(22, 83);
 
   wall.shapeColor="green";
 
